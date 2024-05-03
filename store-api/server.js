bc7ae5e0ba0fcc6c989f85be0520eebce9aa2a71
devsutong <ymphaidiensutong99@gmail.com>
2024-05-03 12:32:15 +0530

    starter server.js

---
+++ store-api/server.js
@@ -0,0 +1,25 @@
+
+const express = require("express");
+
+const app = express();
+
+app.use(express.json)
+
+const PORT = process.env.PORT || 3000;
+
+// Route Inports
+const AuthorizationRoutes = require("");
+const UserRotes = require("");
+const ProductRoutes = require("");
+
+//Sequealize model imports
+const UserModel = require("");
+const ProductModel = require("");
+
+app.listen(PORT, () => {
+    console.log("Server Listening on PORT:", PORT)
+});
+
+app.get("/status", () => {
+    comsole
+})
