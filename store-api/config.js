b18f5c94484fe03516bdd1dfbaddfbbca2c8a4a2
devsutong <ymphaidiensutong99@gmail.com>
2024-05-04 23:44:02 +0530

    register

---
+++ store-api/config.js
@@ -0,0 +1,12 @@
+module.export = {
+    port: 3000,
+    jwtSecret: 'pass',
+    jwtExpirationInSeconds: 60 * 60,
+    roles: {
+        USER: 'user',
+        ADMIN: 'admin'
+    },
+    productPriceUnits: {
+        INR: 'inr'
+    }
+}
