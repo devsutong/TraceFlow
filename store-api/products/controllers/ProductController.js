70e1d0e852c4f261fb2aeb7669bc71f23d075387
devsutong <ymphaidiensutong99@gmail.com>
2024-05-22 10:37:52 +0530

    create product upload

--- store-api/products/controllers/ProductController.js
+++ store-api/products/controllers/ProductController.js
@@ -1,42 +1,60 @@
-const ProductModel  = require("../../common/models/Product");
+const { initialise, createProduct, findProduct, updateProduct, findAllProducts, deleteProduct } = require("../../common/models/Product");
+const { findAllCategories } = require("../../common/models/Category");
+
 
 module.exports = {
-    createProduct: (req, res) => {
-        const payload = req.body;
-        ProductModel.createProduct(payload)
-        .then((product) => {
-            return res.status(201).json({
-                status: true,
-                data: product.toJSON()
-            });
-        })
-        .catch((error) => {
+    createProduct: async (req, res) => {
+        const { name, description, price, image, categoryIds } = req.body;
+        const priceUnit = req.body.priceUnit || "inr";
+        try {
+            const product = await createProduct({ name, description, image, price, priceUnit });
+
+            if (categoryIds && categoryIds.length > 0) {
+                console.log("ok")
+                const categories = await findAllCategories({ id: categoryIds });
+                console.log(categories)
+                await product.addCategories(categories);
+                const productWithCategories = await findProduct(product.id, { include: categories});
+                console.log(this.Category)
+
+                return res.status(201).json({
+                    status: true,
+                    data: productWithCategories.toJSON()
+                });
+            } else {
+                console.log("NO Categories");
+                return res.status(201).json({
+                    status: true,
+                    data: product.toJSON()
+                });
+            }
+        } catch (error) {
             return res.status(400).json({
                 status: false,
                 error: error.message
             });
-        });
+        }
     },
-    getProducts: (req, res) => {
-        ProductModel.findAllProducts()
-        .then((products) => {
+    getProducts: async (req, res) => {
+        const { query: filters } = req;
+        try {
+            const products = await findAllProducts(filters);
             return res.status(200).json({
                 status: true,
                 data: products.map((product) => product.toJSON())
             });
-        })
-        .catch((error) => {
+        } catch (error) {
             return res.status(400).json({
                 status: false,
                 error: error.message
             });
-        });
+        }
     },
-    getProduct: (req, res) => {
+    getProduct: async (req, res) => {
         const { id } = req.params;
-        ProductModel.findProduct({ id })
-        .then((product) => {
-            if(!product) {
+        try {
+            const product = await findProduct({ id });
+            if (!product) {
                 return res.status(404).json({
                     status: false,
                     error: "Product not found!"
@@ -46,52 +64,50 @@ module.exports = {
                 status: true,
                 data: product.toJSON()
             });
-        })
-        .catch((error) => {
+        } catch (error) {
             return res.status(400).json({
                 status: false,
                 error: error.message
             });
-        });
+        }
     },
-    updateProduct: (req, res) => {
+    updateProduct: async (req, res) => {
         const { id } = req.params;
         const payload = req.body;
-        
+
         if (!Object.keys(payload).length) {
             return res.status(400).json({
                 status: false,
-                error: "Body is empty, hence can not update the product."
+                error: "Body is empty, hence cannot update the product."
             });
         }
-        ProductModel.updateProduct({ id }, payload)
-        .then((product) => {
+        try {
+            await updateProduct({ id }, payload);
+            const updatedProduct = await findProduct({ id });
             return res.status(200).json({
                 status: true,
-                data: product.toJSON()
+                data: updatedProduct.toJSON()
             });
-        })
-        .catch((error) => {
+        } catch (error) {
             return res.status(400).json({
                 status: false,
                 error: error.message
             });
-        });
+        }
     },
-    deleteProduct: (req, res) => {
+    deleteProduct: async (req, res) => {
         const { id } = req.params;
-        ProductModel.deleteProduct({ id })
-        .then(() => {
+        try {
+            await deleteProduct({ id });
             return res.status(200).json({
                 status: true,
                 data: "Product deleted successfully!"
             });
-        })
-        .catch((error) => {
+        } catch (error) {
             return res.status(400).json({
                 status: false,
                 error: error.message
             });
-        });
-    }
+        }
+    },
 };
