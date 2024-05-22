60a1d2693d95e6ea556d9af3e4b2333f84c46df2
devsutong <ymphaidiensutong99@gmail.com>
2024-05-22 10:34:15 +0530

    modular sequelize instance

---
+++ store-api/common/models/SequelizeInstance.js
@@ -0,0 +1,13 @@
+const Sequelize = require("sequelize");
+
+const dbHost = process.env.DB_HOST;
+const dbUser = process.env.DB_USER;
+const dbPass = process.env.DB_PASS;
+const dbName = process.env.DB_NAME;
+
+const sequelize = new Sequelize(dbName, dbUser, dbPass,{
+    host: dbHost,
+    dialect: "mysql"
+  });
+
+module.exports = sequelize;
