4f3135726b16dd43e688f21c40965f1783393858
Ethel1412 <ethelrani.lucario@gmail.com>
2024-08-19 13:44:54 +0530

    aaaa

---
+++ store-frontend/src/components/Products/ProductCard.js
@@ -0,0 +1,17 @@
+// src/components/ProductCard.js
+import React from 'react';
+import './styles/ProductCard.css'; // You can keep the CSS specific to this component
+
+const ProductCard = ({ product }) => {
+  return (
+    <div className="product-card">
+      <img src={product.image} alt={product.name} className="product-image" />
+      <h3>{product.name}</h3>
+      <p>{product.description}</p>
+      <p>${product.price} per {product.priceUnit}</p>
+      <button className="buy-now-btn">Add to cart</button>
+    </div>
+  );
+};
+
+export default ProductCard;
